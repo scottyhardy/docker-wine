c4415436d4011f3f19ea86f4ff4e33f9b5310de2
Scott Hardy <cowmonger69@hotmail.com>
2017-02-04 17:09:02 +1100

    Reduced back to single RUN command and added some doco to entrypoint.sh (#5)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# $HOME = "/wine" - set by Dockerfile
+# $USER = username of user who was passed to container via:
+#           `docker run --env="USER"` ...
+
 chown -R $USER:$USER $HOME
 ln -s $HOME /home/$USER
 if [ $# == 0 ]; then
