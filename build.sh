1afc09757e48e95c52511769f0c733e0d5553eb1
Christopher Wicklein <chrisw@wicklein.org>
2019-11-02 11:39:48 -0500

    use buildah if it is available and docker is not

--- build.sh
+++ build.sh
@@ -10,9 +10,20 @@ fi
 BUILD_TARGET="$1"
 VERSION=$(cat ./VERSION)
 
+BUILDER="docker build"
+if ! ${BUILDER%% *} -v >/dev/null 2>&1; then
+    BUILDER="buildah bud"
+    if ! ${BUILDER%% *} -v >/dev/null 2>&1; then
+        echo "Didn't find docker or buildah."
+        exit 1
+    else
+        echo "Didn't find docker, did find buildah, using buildah."
+    fi
+fi
+
 source ./build_args/${BUILD_TARGET}
 
-docker build \
+$BUILDER \
     --build-arg BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ') \
     --build-arg GECKO_VER=$GECKO_VER \
     --build-arg GIT_REV=$(git rev-parse HEAD) \
