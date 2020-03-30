f89ae957b0624e66184c1ce885714cd17fe54766
scottyhardy <scotthardy42@outlook.com>
2020-03-30 22:22:15 +1100

    fix: errors with using rdp and setting password

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@
 USER_NAME="${USER_NAME:-wineuser}"
 USER_UID="${USER_UID:-1010}"
 USER_GID="${USER_GID:-$USER_UID}"
-USER_HOME="${USER_HOME:-/home/$USER}"
-USER_PASSWD="${USER_PASSWORD:-$USER}"
+USER_HOME="${USER_HOME:-/home/$USER_NAME}"
+USER_PASSWD="${USER_PASSWD:-$USER_NAME}"
 RDP_SERVER="${RDP_SERVER:-no}"
 RUN_AS_ROOT="${RUN_AS_ROOT:-no}"
 
 # Create the user account
-groupadd --gid "${USER_GID}" "${USER}"
-useradd --shell /bin/bash --uid 1010 --gid 1010 --password $(openssl passwd "${USER_PASSWORD}") --no-create-home --home-dir "${USER_HOME}" "${USER}"
+groupadd --gid "${USER_GID}" "${USER_NAME}"
+useradd --shell /bin/bash --uid 1010 --gid 1010 --password $(openssl passwd "${USER_PASSWD}") --no-create-home --home-dir "${USER_HOME}" "${USER_NAME}"
 
 # Create the user's home if it doesn't exist
 [ ! -d "${USER_HOME}" ] && mkdir -p "${USER_HOME}"
@@ -27,7 +27,7 @@ if echo "${RDP_SERVER}" | grep -q -i -E '^(no|off|false|0)$'; then
     # Set up pulseaudio for redirection to UNIX socket
     cp /root/pulse/client.conf /etc/pulse/client.conf
 
-    # Run in X11 redirection mode as $USER (default)
+    # Run in X11 redirection mode as $USER_NAME (default)
     if echo "${RUN_AS_ROOT}" | grep -q -i -E '^(no|off|false|0)$'; then
 
         # Copy and take ownership of .Xauthority for X11 redirection
@@ -37,7 +37,7 @@ if echo "${RDP_SERVER}" | grep -q -i -E '^(no|off|false|0)$'; then
         fi
 
         # Run in X11 redirection mode as user
-        exec gosu "${USER}" "$@"
+        exec gosu "${USER_NAME}" "$@"
 
     # Run in X11 redirection mode as root
     elif echo "${RUN_AS_ROOT}" | grep -q -i -E '^(yes|on|true|1)$'; then
@@ -60,7 +60,7 @@ elif echo "${RDP_SERVER}" | grep -q -i -E '^(yes|on|true|1)$'; then
         /usr/sbin/xrdp
 
         if echo "${RUN_AS_ROOT}" | grep -q -i -E '^(no|off|false|0)$'; then
-            exec gosu "${USER}" "$@"
+            exec gosu "${USER_NAME}" "$@"
         elif echo "${RUN_AS_ROOT}" | grep -q -i -E '^(yes|on|true|1)$'; then
             exec "$@"
         else
