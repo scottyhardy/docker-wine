c8b28ac88e9fccd1e2d4e8710d29cf3ca6909931
scottyhardy <scotthardy42@outlook.com>
2020-04-01 11:50:03 +1100

    feat: scrape winehq download page and fetch mono and gecko installers

--- Dockerfile
+++ Dockerfile
@@ -32,6 +32,10 @@ RUN wget -nv https://raw.githubusercontent.com/Winetricks/winetricks/master/src/
         -O /usr/bin/winetricks \
     && chmod +rx /usr/bin/winetricks
 
+# Download gecko and mono installers
+COPY download_gecko_and_mono.sh /root/download_gecko_and_mono.sh
+RUN /root/download_gecko_and_mono.sh
+
 COPY pulse-client.conf /root/pulse/client.conf
 COPY entrypoint.sh /usr/bin/entrypoint
 ENTRYPOINT ["/usr/bin/entrypoint"]
---
+++ download_gecko_and_mono.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Scrapes the WineHQ download pages for latest versions of mono and gecko to download
+
+get_hrefs () {
+    local url="$1"
+    local regexp="$2"
+
+    wget -q -O - "${url}" | sed -E 's/></>\n</g' | sed -n -E "s|^.*<a href=\"(${regexp})\">.*|\1|p" | uniq
+}
+
+for APP in "gecko" "mono"; do
+
+    URL="http://dl.winehq.org/wine/wine-${APP}/"
+
+    # Get the latest version
+    VER=$(get_hrefs "${URL}" '[0-9]+(\.[0-9]+)*/' | sed -E 's|/$||' | sort -rV | head -1)
+
+    # Get the list of files to download
+    mapfile -t FILES < <(get_hrefs "${URL}${VER}/" '.*\.msi')
+
+    # Download the files
+    [ ! -d "/usr/share/wine/${APP}" ] && mkdir -p "/usr/share/wine/${APP}"
+    for FILE in "${FILES[@]}"; do
+        echo "Downloading '${FILE}'"
+        wget -nv "${URL}${VER}/${FILE}" -O "/usr/share/wine/${APP}/${FILE}"
+    done
+done
