96b17430179513dbab0c4d22d2b5134a2cfe7448
scottyhardy <scotthardy42@outlook.com>
2020-04-03 22:02:47 +1100

    refactor: double quotes for consistency

--- download_gecko_and_mono.sh
+++ download_gecko_and_mono.sh
@@ -6,7 +6,7 @@ get_hrefs () {
     local url="$1"
     local regexp="$2"
 
-    wget -q -O - "${url}" | sed -E 's/></>\n</g' | sed -n -E "s|^.*<a href=\"(${regexp})\">.*|\1|p" | uniq
+    wget -q -O- "${url}" | sed -E "s/></>\n</g" | sed -n -E "s|^.*<a href=\"(${regexp})\">.*|\1|p" | uniq
 }
 
 for APP in "gecko" "mono"; do
@@ -14,10 +14,10 @@ for APP in "gecko" "mono"; do
     URL="http://dl.winehq.org/wine/wine-${APP}/"
 
     # Get the latest version
-    VER=$(get_hrefs "${URL}" '[0-9]+(\.[0-9]+)*/' | sed -E 's|/$||' | sort -rV | head -1)
+    VER=$(get_hrefs "${URL}" "[0-9]+(\.[0-9]+)*/" | sed -E "s|/$||" | sort -rV | head -1)
 
     # Get the list of files to download
-    mapfile -t FILES < <(get_hrefs "${URL}${VER}/" '.*\.msi')
+    mapfile -t FILES < <(get_hrefs "${URL}${VER}/" ".*\.msi")
 
     # Download the files
     [ ! -d "/usr/share/wine/${APP}" ] && mkdir -p "/usr/share/wine/${APP}"
