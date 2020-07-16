a383fd7df1465ecc320d6cd5f880057792c7da46
scottyhardy <scotthardy42@outlook.com>
2020-07-16 13:04:04 +1000

    refactor: use functions to determine if options enabled/disabled

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+is_enabled () {
+    echo "$1" | grep -q -i -E "^(yes|on|true|1)$"
+}
+
+is_disabled () {
+    echo "$1" | grep -q -i -E "^(no|off|false|0)$"
+}
+
+
 # Set user account and run values
 USER_NAME=${USER_NAME:-wineuser}
 USER_UID=${USER_UID:-1010}
@@ -10,7 +19,7 @@ RDP_SERVER=${RDP_SERVER:-no}
 RUN_AS_ROOT=${RUN_AS_ROOT:-no}
 FORCED_OWNERSHIP=${FORCED_OWNERSHIP:-no}
 TZ=${TZ:-UTC}
-XVFB=${XVFB:-no}
+USE_XVFB=${USE_XVFB:-no}
 
 # Create the user account
 ! grep -q ":${USER_GID}:$" /etc/group && groupadd --gid "${USER_GID}" "${USER_NAME}"
@@ -22,7 +31,7 @@ useradd --shell /bin/bash --uid "${USER_UID}" --gid "${USER_GID}" --password "${
 # Take ownership of user's home directory if owned by root or if FORCED_OWNERSHIP is enabled
 OWNER_IDS="$(stat -c "%u:%g" "${USER_HOME}")"
 if [ "${OWNER_IDS}" != "${USER_UID}:${USER_GID}" ]; then
-    if [ "${OWNER_IDS}" == "0:0" ] || [ "${FORCED_OWNERSHIP}" == "yes" ]; then
+    if [ "${OWNER_IDS}" == "0:0" ] || is_enabled "${FORCED_OWNERSHIP}"; then
         chown -R "${USER_UID}":"${USER_GID}" "${USER_HOME}"
     else
         echo "ERROR: User's home '${USER_HOME}' is currently owned by $(stat -c "%U:%G" "${USER_HOME}")"
@@ -35,21 +44,21 @@ fi
 ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime && echo "${TZ}" > /etc/timezone
 
 # Run in X11 redirection mode (default) or with xvfb
-if echo "${RDP_SERVER}" | grep -q -i -E "^(no|off|false|0)$"; then
+if echo is_disabled "${RDP_SERVER}"; then
 
     # Set up pulseaudio for redirection to UNIX socket
     [ -f /root/pulse/client.conf ] && cp /root/pulse/client.conf /etc/pulse/client.conf
 
     # Run xvfb
-    if echo "${XVFB}" | grep -q -i -E "^(yes|on|true|1)$"; then
+    if is_enabled "${USE_XVFB}"; then
         nohup /usr/bin/Xvfb "${XVFB_SERVER}" -screen "${XVFB_SCREEN}" "${XVFB_RESOLUTION}" >/dev/null 2>&1 &
     fi
 
     # Run in X11 redirection mode as $USER_NAME (default)
-    if echo "${RUN_AS_ROOT}" | grep -q -i -E "^(no|off|false|0)$"; then
+    if is_disabled "${RUN_AS_ROOT}"; then
 
         # Copy and take ownership of .Xauthority for X11 redirection
-        if [ -f /root/.Xauthority ] && [ "${XVFB}" == "no" ]; then
+        if [ -f /root/.Xauthority ] && is_disabled "${USE_XVFB}"; then
             cp /root/.Xauthority "${USER_HOME}"
             chown "${USER_UID}":"${USER_GID}" "${USER_HOME}/.Xauthority"
         fi
@@ -58,7 +67,7 @@ if echo "${RDP_SERVER}" | grep -q -i -E "^(no|off|false|0)$"; then
         exec gosu "${USER_NAME}" "$@"
 
     # Run in X11 redirection mode as root
-    elif echo "${RUN_AS_ROOT}" | grep -q -i -E "^(yes|on|true|1)$"; then
+    elif is_enabled "${RUN_AS_ROOT}"; then
         exec "$@"
     else
         echo "ERROR: '${RUN_AS_ROOT}' is not a valid value for RUN_AS_ROOT"
@@ -66,7 +75,7 @@ if echo "${RDP_SERVER}" | grep -q -i -E "^(no|off|false|0)$"; then
     fi
 
 # Run in RDP server mode
-elif echo "${RDP_SERVER}" | grep -q -i -E "^(yes|on|true|1)$"; then
+elif is_enabled "${RDP_SERVER}"; then
 
     # Exit if using nordp image
     if ! [ -f /usr/sbin/xrdp ]; then
@@ -83,9 +92,9 @@ elif echo "${RDP_SERVER}" | grep -q -i -E "^(yes|on|true|1)$"; then
     else
         /usr/sbin/xrdp
 
-        if echo "${RUN_AS_ROOT}" | grep -q -i -E "^(no|off|false|0)$"; then
+        if is_disabled "${RUN_AS_ROOT}"; then
             exec gosu "${USER_NAME}" "$@"
-        elif echo "${RUN_AS_ROOT}" | grep -q -i -E "^(yes|on|true|1)$"; then
+        elif is_enabled "${RUN_AS_ROOT}"; then
             exec "$@"
         else
             echo "ERROR: '${RUN_AS_ROOT}' is not a valid value for RUN_AS_ROOT"
