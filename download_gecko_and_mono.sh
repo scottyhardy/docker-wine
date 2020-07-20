ebfb21484c8bc1521877275fb60019e04b2b11ca
scottyhardy <scotthardy42@outlook.com>
2020-07-20 19:36:41 +1000

    refactor: revert to quiet wget in funcs and include APP_VERSION in APP_URL var

--- download_gecko_and_mono.sh
+++ download_gecko_and_mono.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# Scrapes the wine source code for versions of mono and gecko to download
+# Scrapes the Wine source code for versions of mono and gecko to download for a given version of Wine
 
 get_hrefs () {
     local url="$1"
     local regexp="$2"
 
-    wget -nv -O- "${url}" | sed -E "s/></>\n</g" | sed -n -E "s|^.*<a href=\"(${regexp})\">.*|\1|p" | uniq
+    wget -q -O- "${url}" | sed -E "s/></>\n</g" | sed -n -E "s|^.*<a href=\"(${regexp})\">.*|\1|p" | uniq
 }
 
 get_app_ver () {
     local app="${1^^}"  # Convert to uppercase
     local url="https://raw.githubusercontent.com/wine-mirror/wine/wine-${WINE_VER}/dlls/appwiz.cpl/addons.c"
 
-    wget -nv -O- "${url}" | grep -E "^#define ${app}_VERSION\s" | awk -F\" '{print $2}'
+    wget -q -O- "${url}" | grep -E "^#define ${app}_VERSION\s" | awk -F\" '{print $2}'
 }
 
 
@@ -32,13 +32,13 @@ for APP in "gecko" "mono"; do
     APP_VER=$(get_app_ver "${APP}")
 
     # Get the list of files to download
-    APP_URL="http://dl.winehq.org/wine/wine-${APP}/"
-    mapfile -t FILES < <(get_hrefs "${APP_URL}${APP_VER}/" ".*\.msi")
+    APP_URL="http://dl.winehq.org/wine/wine-${APP}/${APP_VER}/"
+    mapfile -t FILES < <(get_hrefs "${APP_URL}" ".*\.msi")
 
     # Download the files
     [ ! -d "/usr/share/wine/${APP}" ] && mkdir -p "/usr/share/wine/${APP}"
     for FILE in "${FILES[@]}"; do
         echo "Downloading '${FILE}'"
-        wget -nv -O "/usr/share/wine/${APP}/${FILE}" "${APP_URL}${APP_VER}/${FILE}"
+        wget -nv -O "/usr/share/wine/${APP}/${FILE}" "${APP_URL}${FILE}"
     done
 done
